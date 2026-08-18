6c7945151f1d2c40ae92857aed60cabb3d9b54fe
Jeongchan-Seo <sjc07020916@gmail.com>
2026-08-18 11:21:13 +0900

    swagger initial setting

---
+++ src/main/java/com/travel/global/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.travel.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TRAVEL API")
+                        .description("TRAVEL 프로젝트 REST API 문서")
+                        .version("0.0.1")
+                );
+    }
+}
