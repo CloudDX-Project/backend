73781a3707f54813df13c281bf89473bc1b34837
Jeongchan-Seo <sjc07020916@gmail.com>
2026-09-09 10:43:10 +0900

    add test config

--- src/test/java/com/travel/TravelApplicationTests.java
+++ src/test/java/com/travel/TravelApplicationTests.java
@@ -2,7 +2,9 @@ package com.travel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class TravelApplicationTests {
 
