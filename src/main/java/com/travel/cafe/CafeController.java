b649f7de90b04a552c5af492e53081ca1f5f5bae
Jeongchan-Seo <sjc07020916@gmail.com>
2026-09-16 11:44:21 +0900

    remove /api/v1

--- src/main/java/com/travel/cafe/CafeController.java
+++ src/main/java/com/travel/cafe/CafeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({"/api/cafes", "/api/v1/cafes"})
+@RequestMapping({"/api/cafes"})
 public class CafeController {
 
     private final CafeService cafeService;
--- src/main/java/com/travel/restaurant/RestaurantController.java
+++ src/main/java/com/travel/restaurant/RestaurantController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({"/api/restaurants", "/api/v1/restaurants"})
+@RequestMapping({"/api/restaurants"})
 public class RestaurantController {
 
     private final RestaurantService restaurantService;
